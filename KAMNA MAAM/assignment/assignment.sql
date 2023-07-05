c08a561627b6e27709a4cf6f24d904b3e99348fb
Darsh Panchal <137559729+heydarsh@users.noreply.github.com>
2023-07-05 23:32:03 +0530

    Add files via upload

---
+++ KAMNA MAAM/assignment/assignment.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE Employee;
+USE Employee;
+
+CREATE TABLE EmploeeTable(
+ emp_id INT PRIMARY KEY,
+ emp_name VARCHAR(100),
+
+
+
+emp_salary INT
+ );
+
+INSERT INTO EmployeeTable VALUES
+(1, 'Rohit Sharma', 10000),
+(2, 'Virat Kohli' , 85000),
+(3, 'Mahendra Singh Dhoni' , 50000);
+
+
+
+
+
